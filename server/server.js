4a130cb8c6a89cb2489f3c1a3c4172e28ddae5d2
max90.dm@gmail.com <cele0504>
2017-04-21 14:06:59 -0300

    - add cors

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ import path from 'path';
 import bodyParser from 'body-parser';
 const app = express();
 
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
